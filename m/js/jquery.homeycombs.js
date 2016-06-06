6efc1dc7bbe70fb4ac3951dd03ee4bf584c6f476
carsnet <niltux@gmail.com>
2016-06-07 00:21:40 +0530

    l

--- m/js/jquery.homeycombs.js
+++ m/js/jquery.homeycombs.js
@@ -4,7 +4,7 @@
 
         // Establish our default settings
         var settings = $.extend({
-            combWidth: 200,
+            combWidth: 150,
             margin: 10
         }, options);
 
